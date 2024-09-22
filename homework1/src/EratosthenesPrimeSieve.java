e727c79bbe0a3442ec802be62bf5f4145c5e88ec
Leonhard Perstling <leonahrd.perstling@gmail.com>
2024-09-22 21:45:08 +0200

    Variables created

--- homework1/src/EratosthenesPrimeSieve.java
+++ homework1/src/EratosthenesPrimeSieve.java
@@ -1,11 +1,14 @@
 public class EratosthenesPrimeSieve implements PrimeSieve {
 
+    private int upperBound;
+    private boolean[] sieve;
+
     public boolean isPrime(int p) {
         return false;
     }
 
 
-    
+
     public void printPrimes() {
 
     }
