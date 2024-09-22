59e14f47384581fab3e89ecdeb52a852d0758bbe
Leonhard Perstling <leonahrd.perstling@gmail.com>
2024-09-22 21:37:31 +0200

    Created all classes

---
+++ homework1/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,12 @@
+public class EratosthenesPrimeSieve implements PrimeSieve {
+
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+
+    
+    public void printPrimes() {
+
+    }
+}
---
+++ homework1/src/PrimeSieve.java
@@ -0,0 +1,8 @@
+
+public interface PrimeSieve {
+
+
+    public boolean isPrime(int p);
+
+    public void printPrimes();
+}
