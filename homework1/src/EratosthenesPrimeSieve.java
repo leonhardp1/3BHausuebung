c8f604f47ae34f8127ab013f47864cdd2657b301
Leonhard Perstling <leonahrd.perstling@gmail.com>
2024-09-23 22:25:22 +0200

    created isPrime and printPrimes

--- homework1/src/EratosthenesPrimeSieve.java
+++ homework1/src/EratosthenesPrimeSieve.java
@@ -1,15 +1,34 @@
-public class EratosthenesPrimeSieve implements PrimeSieve {
+import java.util.Scanner;
 
+public class EratosthenesPrimeSieve implements PrimeSieve {
     private int upperBound;
     private boolean[] sieve;
 
+    public EratosthenesPrimeSieve(int upperBound) {
+        this.upperBound = upperBound;
+        this.sieve = new boolean[upperBound + 1];
+    }
+
+
+
+    @Override
     public boolean isPrime(int p) {
-        return false;
+        if (p < 2 || p > upperBound) {
+            return false;
+        }
+        return sieve[p];
     }
 
-
-
+    @Override
     public void printPrimes() {
-
+        System.out.println("Primzahlen bis " + upperBound + ":");
+        for (int i = 2; i <= upperBound; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
+
+
 }
